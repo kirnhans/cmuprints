3f76ee1361a892857d918142aa2bf75d7fb50774
kkleiven <kkleiven@andrew.cmu.edu>
2014-11-24 14:12:58 -0500

    fixed updateData function

--- index/models.py
+++ index/models.py
@@ -6,88 +6,35 @@ import string
 from printerSetup import getPrinterList
 
 class PrinterList(models.Model):
-    plist=printerList
+    def __init__(self):
+        self.plist=getPrinterList()
+        super(PrinterList, self).__init__()
 
-Printers = PrinterList()
+    def getInfo(self):
+        req = urllib2.Request('http://198.211.113.33:3000/printers')
+        response = urllib2.urlopen(req)
+        the_page = response.read()
+        return the_page
 
-def getInfo():
-    info = urllib2.urlopen('http://198.211.113.33:3000/printers')
-    str_info = ''
-    for line in info:
-        str_info += line
-    return str_info
-
-def updateData():
-    encoded_object = getInfo()
-    myobj_instance = MyDecoder().decode(encoded_object)
-
-class Printer(object):
-    def __init__(self, name):
-        self.fullName = name
-        self.parseName(name)
-        self.status = None
-        self.error = None
-        self.icon = None
-
-    def parseName(self, name):
-        start = name.find('-')
-        if start == -1:
-            self.name = "Name Error"
-        else:
-            name = name[start+2:]
-            self.name = name
-            if name.endswith("B&W"):
-                #self.name = name[:len(name)-len("B&W")]
-                self.color = False
-            else:
-                #self.name = name[:len(name)-len("Color")]
-                self.color = True
-
-    def __repr__(self):
-        return "%s: %s (%s)" % (self.name, self.icon, self.error)
-        
-    def setIcon(self, msg):
-        if msg == "go.gif":
-            status = "available"
-        elif msg == "yield.gif":
-            status = "warning"
-        elif msg == "stop.gif":
-            status = "not working"
-        else:
-            status = "unknown"
-        self.icon = status
-    def getIcon(self):
-        return self.icon
-    
-    def setStatus(self, msg):
-        self.status = msg
-    def getStatus(self):
-        return self.status
-
-    def setErrorMessage(self, msg):
-        self.error = msg
-    def getErrorMessage(self):
-        return self.error
-
+    def updateData(self):
+        encoded_object = self.getInfo()
+        myobj_instance = MyDecoder(self.plist).decode(encoded_object)
 
 class MyDecoder(json.JSONDecoder):
-    def __init__(self):
+    def __init__(self, plist):
+        self.plist = plist
         json.JSONDecoder.__init__(self, object_hook=self.dict_to_object)
 
     def dict_to_object(self, d):
         if 'name' in d:
             fullName = d.pop('name')
-            for printer in printerList:
+            for printer in self.plist:
                 if printer == fullName:
                     if 'ready' in d:
                         printer.setStatus(d.pop('ready'))
                     if 'icon' in d:
                         printer.setIcon(d.pop('icon'))
-                    if 'status' in d:
-                        printer.setErrorMessage(d.pop('status'))
+                    if 'message' in d:
+                        printer.setErrorMessage(d.pop('message'))
 
-encoded_object = getInfo()
-myobj_instance = MyDecoder().decode(encoded_object)
-
-for printer in Printers.plist:
-    print printer.status
+Printers = PrinterList()
